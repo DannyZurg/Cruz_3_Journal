c9d8f3e8b472230900775443db54f1360191e7b6
D22 <DannyZurg@ninja14.d22.lan>
2015-12-15 11:00:06 -0600

    This is my MethodMadness.java

--- Cruz_3_MethodMadness.java
+++ Cruz_3_MethodMadness.java
@@ -9,48 +9,58 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Cruz_3_MethodMAdness extends Application {
-    
+
 public static void main(String[] args) {
 launch(args);
 }
 
-@Override 
+@Override
 public void start(Stage primaryStage) {
 primaryStage.setTitle("Drawing Operations Test");
 Group root = new Group();
 Canvas canvas = new Canvas(500, 500);
 GraphicsContext gc = canvas.getGraphicsContext2D();
-drawShape(gc);
+drawCircle1(gc);
+drawCircle2(gc);
+drawCircle3(gc);
+drawCircle4(gc);
+drawCircle5(gc);
+drawStar(gc);
 root.getChildren().add(canvas);
 primaryStage.setScene(new Scene(root));
 primaryStage.show();
 }
 
-private void drawShape(GraphicsContext gc) {
+private void drawCircle1(GraphicsContext gc) {
 
     gc.setFill(Color.GOLD);
     gc.fillRoundRect(210, 90, 75, 70, 150, 170);
     gc.setStroke(Color.GOLD);
-    
+private void drawStar(GraphicsContext gc)  {
     gc.setFill(Color.GOLD);
-    gc.fillPolygon(new double[]{250,200,150,200,150,250,350,300,350,300}, 
+    gc.fillPolygon(new double[]{250,200,150,200,150,250,350,300,350,300},
             new double[]{150,200,200,250,300,275,300,250,200,200}, 10);
-    gc.setStroke(Color.GOLD); 
-    
-      gc.setFill(Color.GOLD);
+    gc.setStroke(Color.GOLD);
+    }
+ private void drawCircle2(GraphicsContext gc) {
+gc.setFill(Color.GOLD);
     gc.fillRoundRect(100, 170, 75, 70, 150, 170);
     gc.setStroke(Color.GOLD);
-    
-      gc.setFill(Color.GOLD);
+    }
+private void drawCircle3(GraphicsContext gc) {
+ gc.setFill(Color.GOLD);
     gc.fillRoundRect(120, 275, 75, 70, 150, 170);
     gc.setStroke(Color.GOLD);
-    
-      gc.setFill(Color.GOLD);
+    }
+private void drawCircle4(GraphicsContext gc) {
+ gc.setFill(Color.GOLD);
     gc.fillRoundRect(300, 275, 75, 70, 150, 170);
     gc.setStroke(Color.GOLD);
-    
-      gc.setFill(Color.GOLD);
+    }
+private void drawCircle5(GraphicsContext gc) {
+ gc.setFill(Color.GOLD);
     gc.fillRoundRect(325, 170, 75, 70, 150, 170);
     gc.setStroke(Color.GOLD);
 }
-}       
+}
+}
